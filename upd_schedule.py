85953b021b882d754ef2a9958368738e2ba37ddd
modenaf360 <wingnut0310@gmail.com>
2019-04-26 00:41:41 +0900

    Add Scheduler update "pip install -U youtube-dl" once a day.

---
+++ upd_schedule.py
@@ -0,0 +1,10 @@
+import subprocess
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def exec_interval():
+    result = subprocess.run(["pip", "install", "-U", "youtube-dl"])
+    print('exec subprocess 1day', result)
+sched = BlockingScheduler()
+sched.add_job(exec_interval, 'interval', seconds=1440)
+
+sched.start()
