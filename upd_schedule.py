d82ee77a462baf89bc65b617b87bfd208c9377f5
hyeonsangjeon <wingnut0310@gmail.com>
2023-02-19 02:42:32 +0900

    Changed the executable from youtube-dl to yt-dlp for fixed error about 'Unable to extract uploader id' and download speed..

--- upd_schedule.py
+++ upd_schedule.py
@@ -2,7 +2,7 @@ import subprocess
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 def exec_interval():
-    result = subprocess.run(["pip", "install", "-U", "youtube-dl"])
+    result = subprocess.run(["pip", "install", "-U", "yt-dlp"])
     print('exec subprocess 1day', result)
 sched = BlockingScheduler()
 sched.add_job(exec_interval, 'interval', seconds=1440)
